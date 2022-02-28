3ce0802e953d5ea1ae5b2542d0c1b8d73ef9007e
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-02-28 13:05:49 +0000

    Add localForage, uuid, routes, basic styling - relates #6 #7
    * Added routes for CreatePage and SingleDoc
    * Added dummy-data.js
    * Added dynamic Routes for my-trips and my-documents
    * Added basic styling in index.css
    * Installed localForage package and uuid package

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./css/index.css";
 // import App from "./App";
 import Home from "./routes/Home";
-import PageNotFound from "./routes/PageNotFound"
+import PageNotFound from "./routes/PageNotFound";
+import Docs from "./routes/Docs";
+import SingleDoc from "./routes/SingleDoc";
+import Trips from "./routes/Trips";
+import SingleTrip from "./routes/SingleTrip";
+import CreateTrip from "./routes/CreateTrip";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />}></Route>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+
+        <Route path="/my-documents" element={<Docs />} />
+
+        <Route path="/my-documents/:documentId" element={<SingleDoc />} />
+
+        <Route path="/my-trips" element={<Trips />} />
+
+        <Route path="/my-trips/:tripId" element={<SingleTrip />} />
+
+        <Route path="/create-trip" element={<CreateTrip />} />
+
         <Route path="*" element={<PageNotFound />} />
-			</Routes>
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <h1>Trip Planner</h1>
+
+      <Link to="/my-documents">DOCUMENTS</Link>
+
+      <br></br>
+
+      <Link to="/my-trips">MY TRIPS</Link>
     </div>
   );
 }
