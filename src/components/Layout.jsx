e3f7c57a388ecfaf23557e913407fac57de3b5ef
MillyP <millypalmer1@hotmail.co.uk>
2022-03-01 17:26:46 +0000

    Added home.png logo and navigationso user can go to home page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import arrowBack from "../assets/images/arrow-back.png";
 import arrowNext from "../assets/images/arrow-next.png";
+import home from "../assets/images/home.png";
 import { useNavigate } from "react-router-dom";
 import styles from "styled-components";
 
@@ -10,6 +11,9 @@ function Layout({ pageTitle }) {
     <nav className="top-navigation">
       <button className={styles.btn} onClick={() => navigate(-1)}>
         <img src={arrowBack} className="arrow-back"></img>
+      </button>
+      <button className={styles.btn} onClick={() => navigate(-1)}>
+        <img src={home} className="home"></img>
       </button>
       <button className={styles.btn} onClick={() => navigate(1)}>
         <img src={arrowNext} className="arrow-next"></img>
