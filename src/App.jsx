6498d5ccdeab94f408e818484767c212413a850b
MillyP <millypalmer1@hotmail.co.uk>
2022-03-02 14:00:05 +0000

    Removed next button, added bottom navigation bar, installed fontAwesome icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Layout from "./components/Layout";
 import "./css/App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Docs from "./routes/Docs";
+import Trips from "./routes/Trips";
 import Home from "./routes/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Layout";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Navigation />
+        <Route path="/Docs" component={Docs} />
+        <Route path="/Trips" component={Trips} />
+      </BrowserRouter>
       <Home />
       <Layout />
     </div>
