1c854b77c831e411f89f9312e0f4d0302935c8fa
orian <77785778+OrianP@users.noreply.github.com>
2022-03-02 17:20:00 +0000

    Edit removeItem function
    * filter the data array to "remove" deleted items
    * write comments
    
    Relates #10

--- useDb.js
+++ useDb.js
@@ -3,47 +3,40 @@ import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function useDb(store) {
+  // set initial state to an object with "status" and "data" props
   const [state, setState] = useState({
     status: "loading",
     data: null,
   });
 
+  // destrucutre variables from state object
   const { status, data } = state;
-  // When the page loads we gather the db into an array and set the state
+
+  // When the component loads: gather the db into an array of objects and set the state
   useEffect(() => {
     const result = [];
     store
       .iterate((value, key) => {
-        /**
-         * IMPORTANT: if you return anything but undefined from this callback
-         * localForage just stops iterating! 🤪
-         */
         value.key = key;
         result.push(value);
       })
       .then(() => {
         setState({ status: "resolved", data: result });
-        // console.log("RESULT FROM useDB", result);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // state to console log
-  useEffect(() => {
-    console.log("STATE FROM USEEFFECT IN useDB", state);
-  }, [state]);
+  // console log in a useEffect to see the state after it has been set
+  // useEffect(() => {
+  //   console.log("STATE FROM USEEFFECT IN useDB", state);
+  // }, [state]);
 
+  // return the array containing all the data (array of objects)
   function getAll() {
     return data;
   }
 
-  /**
-   * After this we're just re-implementing stuff localForage has
-   * e.g. "get a thing", "set a thing" etc
-   * The only difference is we keep our internal state up-to-date
-   * so our components always know what's up
-   */
-
-  // get item from store db and set state to equal that item
+  // get item from db and update item state
   function getItem(key, setState) {
     let item = "";
     store
@@ -55,22 +48,24 @@ export default function useDb(store) {
       .catch((err) => console.log(err));
   }
 
-  // insert single item in database
+  // insert single item into db and update state
   function setItem(name, fileUrl) {
     let randomKey = uuidv4();
 
     setState({ status: "updating", data });
-    // if I have an image loaded and user entered a file name
-    if (data && name) {
+    // if name of doc and file were inputed by user
+    if (name && fileUrl) {
       store
         .setItem(randomKey, { name, fileUrl })
         .then(() => {
           console.log(`File ${name} inserted`);
 
-          // have to create a new Map to avoid mutating old React state
+          // spread current data array to avoid mutating old React state
           const newData = [...data];
+          // push the new item into newData array
           newData.push({ key: randomKey, name: name, data: fileUrl });
 
+          // update the state to include new item
           // setState is async
           setState({ status: "resolved", data: newData });
         })
@@ -80,21 +75,25 @@ export default function useDb(store) {
     }
   }
 
-  // remove item
-  // function removeItem(key) {
-  //   setState({ status: "updating", data: state.data });
-  //   store.removeItem(key).then(() => {
-  //     const newData = new Map(data);
-  //     newData.delete(key);
-  //     setState({ status: "resolved", data: newData });
-  //   });
-  // }
+  // remove item and update state
+  function removeItem(key) {
+    setState({ status: "updating", data: state.data });
+    store.removeItem(key).then(() => {
+      const newData = [...data];
+      const filtered = newData.filter((obj) => {
+        console.log("filtering");
+        return obj.key !== key;
+      });
+      setState({ status: "resolved", data: filtered });
+    });
+  }
 
   return {
     status,
     getItem,
     setItem,
     getAll,
+    removeItem,
     state,
   };
 }
