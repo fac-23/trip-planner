5c1902a97ddf4c00ac9e06be9f9348f31b59b402
orian <77785778+OrianP@users.noreply.github.com>
2022-03-03 10:36:48 +0000

    Remove unused functions
    Co-authored-by: Paolo <15671939+paologhidoni@users.noreply.github.com>
    Co-authored-by: Milly <62570785+millipede-cpu@users.noreply.github.com>
    
    Closes #22

--- helper-functions.js
+++ helper-functions.js
@@ -1,11 +1,3 @@
-// import uuid, to generate random ids
-import { v4 as uuidv4 } from "uuid";
-
-// function to set the fileName state to whatever the user enters in the file name input field
-function nameInputHandler(event, setState) {
-  setState(event.target.value);
-}
-
 // function to set the image state to the image uploaded in the reader
 function imageInputHandler(event, setState) {
   // if we have a loaded image
@@ -26,109 +18,42 @@ function imageInputHandler(event, setState) {
   }
 }
 
-// function to insert file in documentsStore, it gets called on form submit
-function insertIntoDb(event, name, data, db, setState) {
-  event.preventDefault();
+// // function to retrieve single file by key from database
+// function getItem(db, key, setSingleFile) {
+//   let singleFile = "";
+//   db.getItem(key)
+//     .then(function (value) {
+//       // This code runs once the value has been loaded
+//       // from the offline store.
+//       // console.log("ITEM RETRIEVED", value.name, value.data);
+//       singleFile = value;
+//     })
+//     .then(function () {
+//       setSingleFile(singleFile);
+//     })
+//     .catch(function (err) {
+//       // This code runs if there were any errors
+//       console.log(err);
+//     });
+// }
 
-  let randomKey = uuidv4();
+// function setSingleFileInitialState(db, key, initialValue) {
+//   let singleFileInitialState = "";
+//   db.getItem(key)
+//     .then(function (value) {
+//       singleFileInitialState = value;
+//     })
+//     .then(function () {
+//       // console.log(
+//       //   "single file initial state in helper functions",
+//       //   singleFileInitialState
+//       // );
+//     })
+//     .catch(function (err) {
+//       // This code runs if there were any errors
+//       console.log(err);
+//     });
+//   return singleFileInitialState || initialValue;
+// }
 
-  // if I have an image loaded and user entered a file name
-  if (data && name) {
-    // add entry to documentsStore
-    db.setItem(randomKey, { name, data })
-      .then(function () {
-        console.log(`File ${name} inserted`);
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-
-    // reset the file name input field
-    setState("");
-  }
-}
-
-// function to delete file from documentsStore
-function deleteFromDb(event, db, elementKey) {
-  // console.log(`clicked ${event.target.value}`);
-
-  // find id of the document we want to remove
-  // const documentId = event.target.value;
-
-  db.removeItem(elementKey)
-    .then(function () {
-      // Run this code once the key has been removed.
-      console.log("Key is cleared!");
-    })
-    .catch(function (err) {
-      // This code runs if there were any errors
-      console.log(err);
-    });
-}
-
-// Function to loop over every entry in localforage and for each one render on the page an li element containing a clickable link which will be the key.
-function renderItemsFromDb(db) {
-  db
-    // eslint-disable-next-line no-unused-vars
-    .iterate(function (value, key, iterationNumber) {
-      // Resulting key/value pair -- this callback
-      // will be executed for every item in the
-      // database.
-      // return [key, value];
-    })
-    .then(function () {
-      console.log("Iteration has completed");
-    })
-    .catch(function (err) {
-      // This code runs if there were any errors
-      console.log(err);
-    });
-}
-
-// function to retrieve single file by key from database
-function getItem(db, key, setSingleFile) {
-  let singleFile = "";
-  db.getItem(key)
-    .then(function (value) {
-      // This code runs once the value has been loaded
-      // from the offline store.
-      // console.log("ITEM RETRIEVED", value.name, value.data);
-      singleFile = value;
-    })
-    .then(function () {
-      setSingleFile(singleFile);
-    })
-    .catch(function (err) {
-      // This code runs if there were any errors
-      console.log(err);
-    });
-}
-
-function setSingleFileInitialState(db, key, initialValue) {
-  let singleFileInitialState = "";
-  db.getItem(key)
-    .then(function (value) {
-      singleFileInitialState = value;
-    })
-    .then(function () {
-      // console.log(
-      //   "single file initial state in helper functions",
-      //   singleFileInitialState
-      // );
-    })
-    .catch(function (err) {
-      // This code runs if there were any errors
-      console.log(err);
-    });
-  return singleFileInitialState || initialValue;
-}
-
-export {
-  nameInputHandler,
-  imageInputHandler,
-  insertIntoDb,
-  deleteFromDb,
-  renderItemsFromDb,
-  setSingleFileInitialState,
-  getItem,
-};
+export { imageInputHandler };
