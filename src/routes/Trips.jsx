0c009c91595c1a92b21e5fcfc553dcdbc3531407
MillyP <millypalmer1@hotmail.co.uk>
2022-03-03 17:10:28 +0000

    Rendering trips on Trips and created StyledTrips component
    Co-authored-by: Orian <77785778+OrianP@users.noreply.github.com>
    
    Co-authored-by: Paolo <15671939+paologhidoni@users.noreply.github.com>

--- src/routes/Trips.jsx
+++ src/routes/Trips.jsx
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
+import trips from "../dummy-data";
 
 // components
 import StyledLink from "../components/StyledLink";
+// import StyledTrips from "../components/StyledTrips";
 import Layout from "../components/Layout";
 
 // images
 import tripicon from "../assets/images/trips-icon.png";
-
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPlus } from "@fortawesome/free-solid-svg-icons";
+console.log("Trip", trips);
 export default function Trips() {
   return (
     <Fragment>
@@ -16,9 +20,23 @@ export default function Trips() {
         <img src={tripicon} alt="icon of a trip" className="trip-icon"></img>
 
         <section className="trips">
-          <StyledLink to="/create-trip">CREATE NEW TRIP</StyledLink>
+          <StyledLink to="/create-trip">
+            CREATE NEW TRIP <strong>+</strong>
+          </StyledLink>
+        </section>
 
-          <ul className="trips__list stack-md"></ul>
+        <section className="trip-card">
+          <ul className="trips__list stack-md">
+            <div>
+              {trips.map((trip) => (
+                <p key={trip.name}>
+                  {trip.name}
+                  {trip.date}
+                </p>
+              ))}
+            </div>
+            {/* <StyledTrips /> */}
+          </ul>
         </section>
       </div>
     </Fragment>
