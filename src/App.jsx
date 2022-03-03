24e24412651e5d69d6136980c33755e06b2010fa
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-03-03 16:43:48 +0000

    Add PdfViewer, implemented global CSS Variables - relates #25 #17
    * inside singledoc.jsx added logic to determine whether file to display is an image or a pdf file.
    * Imported Wiewer and Web Worker, implemented Pdf viewer
    * Imported createGlobalStyle from styled components inside App.jsx, created GlobalStyles component and called it inside App component to make variables available globally.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./css/App.css";
+
+// Import React Router packages
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import Pages components
 import Home from "./routes/Home";
 import PageNotFound from "./routes/PageNotFound";
 import Docs from "./routes/Docs";
@@ -9,6 +15,17 @@ import SingleDoc from "./routes/SingleDoc";
 import Trips from "./routes/Trips";
 import SingleTrip from "./routes/SingleTrip";
 import CreateTrip from "./routes/CreateTrip";
+
+// Import global styles - CSS variables
+import { createGlobalStyle } from "styled-components";
+const GlobalStyles = createGlobalStyle`
+  html {
+    --color-font: #333;
+    --color-primary: #00b9ad;
+    --color-primary-light: #b3eae7;
+  }
+`;
+
 // import localForage, library to use IndexedDB
 import localforage from "localforage";
 
@@ -20,6 +37,7 @@ let documentsStore = localforage.createInstance({
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <BrowserRouter>
         <Routes>
           <Route data-testid="home" path="/" element={<Home />}></Route>
