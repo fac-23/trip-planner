a46d8edd6c70f10c9d84bb2e613990b5ac2ed1f5
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-03-07 11:42:41 +0000

    Add media query to styledImgPreview component - relates #42
    * Added media query to target smaller devices like tablets and phones and limit the image width to 70% of its container.

--- src/components/StyledImgPreview.jsx
+++ src/components/StyledImgPreview.jsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 const StyledImgPreview = styled.img`
   width: 100%;
   max-width: 90%;
+
+  @media screen and (max-width: 860px) {
+    max-width: 70%;
+  }
 `;
 
 export default StyledImgPreview;
