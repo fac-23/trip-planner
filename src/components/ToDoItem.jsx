43f944765351f59e922e5982b8c26bfc01a6bdbd
orian <77785778+OrianP@users.noreply.github.com>
2022-03-08 19:58:55 +0000

    Pass more props into ToDoItem components
    * toDoListStore
    * itemKey
    * itemName
    * isCompleted
    
    Relates #51

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,14 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
-export default function ToDoItem({ itemName }) {
+export default function ToDoItem({
+  toDoListStore,
+  itemKey,
+  itemName,
+  isCompleted,
+}) {
+  const [readOnly, setReadOnly] = useState(false);
+
   return (
     <Fragment>
       <div>
-        <input type="checkbox" />
-        <input type="text" defaultValue={itemName} />
+        <input type="checkbox" defaultChecked={isCompleted} />
+        <input type="text" defaultValue={itemName} readOnly={readOnly} />
       </div>
       <div>
-        <button>Save</button>
+        <button onClick={() => setReadOnly(!readOnly)}>
+          {readOnly ? "Edit" : "Save"}
+        </button>
         <button>Delete</button>
       </div>
     </Fragment>
