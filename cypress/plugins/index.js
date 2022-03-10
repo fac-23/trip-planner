ec5c2ce9287afaa1c65112df6d3d44cc62552330
MillyP <millypalmer1@hotmail.co.uk>
2022-03-10 13:05:00 +0000

    Added test to check if pdf file uploaded, installed cypress-downloadfile

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -16,7 +17,21 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+const { downloadfile } = require("cypress-downloadfile/lib/addPlugin");
 module.exports = (on, config) => {
+  on("task", { downloadfile });
+  return config;
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
+
+context("downloadFile", () => {
+  it("downloaded pdf file", () => {
+    cy.downloadFile(
+      "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      "Downloads",
+      "dummy.pdf"
+    );
+    cy.readFile("src/assets/images/dummy.pdf");
+  });
+});
